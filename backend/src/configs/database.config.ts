18cb8bfd4d2fcc7c07925be35a5bc9b41da5f301
Badea-pro <79918603+Badea-pro@users.noreply.github.com>
2022-12-01 23:38:09 +0300

    Update

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,10 +1,13 @@
 import { connect, ConnectOptions } from "mongoose";
 
 export const dbConnect = () => {
-  connect(process.env.MONGO_URI!, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions).then(
+  connect(
+    "mongodb+srv://Badea:Badea@cluster0.frksb2p.mongodb.net/FoodMine?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions
+  ).then(
     () => console.log("Connect successfully"),
     (error) => console.log(error)
   );
