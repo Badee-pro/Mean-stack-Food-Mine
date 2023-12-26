67c1d979427dccdf95aa7ff81f9dc6d12f7dacae
Badee Khalbouss <79918603+Badee-pro@users.noreply.github.com>
2023-12-26 01:10:56 -0500

    New edit

--- backend/src/models/order.model.ts
+++ backend/src/models/order.model.ts
@@ -1,6 +1,6 @@
-import { model, Schema, Types } from "mongoose";
-import { OrderStatus } from "../constants/order_status";
-import { Food, FoodSchema } from "./food.model";
+import {model, Schema, Types} from 'mongoose';
+import {OrderStatus} from '../constants/order_status';
+import {Food, FoodSchema} from './food.model';
 
 export interface LatLng {
   lat: string;
@@ -8,8 +8,8 @@ export interface LatLng {
 }
 
 export const LatLngSchema = new Schema<LatLng>({
-  lat: { type: String, required: true },
-  lng: { type: String, required: true },
+  lat: {type: String, required: true},
+  lng: {type: String, required: true},
 });
 
 export interface OrderItem {
@@ -19,9 +19,9 @@ export interface OrderItem {
 }
 
 export const OrderItemSchema = new Schema<OrderItem>({
-  food: { type: FoodSchema, required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
+  food: {type: FoodSchema, required: true},
+  price: {type: Number, required: true},
+  quantity: {type: Number, required: true},
 });
 
 export interface Order {
@@ -40,14 +40,14 @@ export interface Order {
 
 const orderSchema = new Schema<Order>(
   {
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    addressLatLng: { type: LatLngSchema, required: true },
-    paymentId: { type: String },
-    totalPrice: { type: Number, required: true },
-    items: { type: [OrderItemSchema], required: true },
-    status: { type: String, default: OrderStatus.NEW },
-    user: { type: Schema.Types.ObjectId, required: true },
+    name: {type: String, required: true},
+    address: {type: String, required: true},
+    addressLatLng: {type: LatLngSchema, required: true},
+    paymentId: {type: String},
+    totalPrice: {type: Number, required: true},
+    items: {type: [OrderItemSchema], required: true},
+    status: {type: String, default: OrderStatus.NEW},
+    user: {type: Schema.Types.ObjectId, required: true},
   },
   {
     timestamps: true,
@@ -60,4 +60,4 @@ const orderSchema = new Schema<Order>(
   }
 );
 
-export const OrderModel = model("order", orderSchema);
+export const OrderModel = model('order', orderSchema);
